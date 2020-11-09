8343b7329af0215140eab646291d1762e851a020
Taimoor-hassan <58907587+Taimoor-hassan@users.noreply.github.com>
2020-11-09 08:27:23 +0500

    f2

--- js/network status.js
+++ js/network status.js
@@ -1,48 +1,21 @@
 
-// /* JS comes here */
-// var status = document.getElementsByClassName("avtar");
+var connected = navigator.onLine;
 
-// window.addEventListener('load', function(e) {
-//     if (navigator.onLine) {
-//         status.style.color = "green";
-//     } else {
-//         status.style.color = "red";
-//     }
-// }, false);
+console.log(connected);
 
-// window.addEventListener('online', function(e) {
-//     status.style.color = "green";
-// }, false);
+if (connected == true) {
+    console.log("You are now back online.");
+    document.querySelector(".fa-user-circle").style.color = "green" ;
 
-// window.addEventListener('offline', function(e) {
-//     status.style.color = "red";
-// }, false);
+} ;
+if (connected == false) {
+    console.log("You lost connection.");
+    document.querySelector(".fa-user-circle").style.color = "red" ;
 
+};
 
-function hasNetwork(online) {
-    const element = document.querySelector(".avatar");
-    // Update the DOM to reflect the current status
-    if (online) {
-        element.classList.remove("offline");
-        element.classList.add("online");
-        element.style.color = "green";
-    } else {
-        element.classList.remove("online");
-        element.classList.add("offline");
-        element.style.color = "red";
-    }
-}
+// var connected = navigator.onLine;
 
-window.addEventListener("load", () => {
-    hasNetwork(navigator.onLine);
+// window.addEventListener('online', () => console.log("You are now back online."), document.querySelector(".fa-user-circle").style.color = "green" );
+// window.addEventListener('offline', () =>  console.log("You lost connection."), document.querySelector(".fa-user-circle").style.color = "red");
 
-    window.addEventListener("online", () => {
-        // Set hasNetwork to online when they change to online.
-        hasNetwork(true);
-    });
-
-    window.addEventListener("offline", () => {
-        // Set hasNetwork to offline when they change to offline.
-        hasNetwork(false);
-    });
-});
